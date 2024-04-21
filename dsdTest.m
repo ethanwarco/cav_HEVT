ec47125789bb12f6076b7619d9506869318f4f77
diegovc121703 <diegovc1217@vt.edu>
2024-04-20 20:56:33 -0400

    Update dsdTest.m

--- dsdTest.m
+++ dsdTest.m
@@ -2,7 +2,7 @@ clear;
 close all;
 clc;
 % create a driving scenario
-scenario = drivingScenario;
+scenario = drivingScenario('SampleTime',0.1,'StopTime',1);
 roadCenters = [0 0 0; 50 0 0];
 road(scenario,roadCenters);
 % create an ego vehicle with waypoints and speed
@@ -13,12 +13,22 @@ smoothTrajectory(egoVehicle,waypoints,speed)
 % create radar and camera sensors
 radar = drivingRadarDataGenerator('MountingLocation',[0 0 0]);
 camera = visionDetectionGenerator('SensorLocation',[0 0],'Yaw',-180);
+
+
 % create a car actor in front of the egoVehicle
-car1 = struct('ActorID',1,'Position',[20 0 0],'Velocity', [30 0 0]);
-car = vehicle(scenario, 'ClassID',1,'Position',[20 0 0]);
+%car1 = struct('ActorID',1,'Position',[20 0 0],'Velocity', [30 0 0]);
+car1 = vehicle(scenario, 'ClassID',1,'Position',[20 0 0]);
 path = [20 0 0; 55 0 0];
-smoothTrajectory(car, path, speed);
+smoothTrajectory(car1, path, speed);
+
 % obtain detection data
-dets = camera(car1, 1.0);
+dets = camera(targetPoses(egoVehicle), scenario.SimulationTime);
+
+
+
+
+
+
+
 % open the scenario in drivingScenarioDesigner
 drivingScenarioDesigner(scenario, {radar, camera});
